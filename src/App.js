cd96099181d049f8f89351150ff4f352b2ae52c6
Roman <fomin.itdev@gmail.com>
2020-02-25 17:02:09 +0300

    clock works online

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Clock from './Clock';
 
-function App() {
-  return (
-    <div className="App">
-      <Clock />
-
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return(
+          <div>
+              <Clock />;
+          </div>
+      )
+  }
 }
 
 export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-// import Moment from 'react-moment';
 
 class Clock extends Component {
-  
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillMount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
     render() {
         return (
             <div>
-               {/* <Moment format="HH:mm:ss" interval={1000} /> */}
+               <h1>{ this.state.date.toLocaleTimeString() }</h1>
             </div>
         )
     }
