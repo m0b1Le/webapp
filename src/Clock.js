9520c871c0181b1ba6da858452248fd016359582
Roman <fomin.itdev@gmail.com>
2020-02-25 16:17:50 +0300

    First update

---
+++ src/Clock.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import Moment from 'react-moment';
+
+class Clock extends Component {
+  
+    render() {
+        return (
+            <div>
+               {/* <Moment format="HH:mm:ss" interval={1000} /> */}
+            </div>
+        )
+    }
+}
+
+export default Clock;
